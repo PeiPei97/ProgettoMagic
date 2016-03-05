a0a489cd63b90a6f882c52603fac80d59b376935
Alessandro Delmastro <delmastro1997@gmail.com>
2016-03-05 15:08:20 +0100

    Inizio scrittura ricerca carte.

--- SqliteQuery/ricerca.php
+++ SqliteQuery/ricerca.php
@@ -41,14 +41,71 @@
 
 
 
-  /*Ricerca le carte, riceve un json contente i campi su cui si vuole effettuare la ricerca.*/
-  /*Formato dati [nomeCarta, tipo, colore, espansione, rarità]*/
-  /*Se il campo non è stato valorizzato dall'utente */
+  /*Ricerca le carte, riceve un vettore contente i campi su cui si vuole effettuare la ricerca.*/
+  /*Formato dati [nome, tipo, colore, espansione, rarita]*/
+  /*Se il campo non è stato valorizzato dall'utente non viene incluso nella ricerca*/
+  function ricerca($request){
+    global $conn;
+    $binds= [];
+    $query = "SELECT * FROM ";
+    $tabelle = "carte";
+    $condizioni = "1 = 1 ";
+    if($request["nome"] != ""){
+      $condizioni = $condizioni."AND nome = ? ";
+      $binds[] = $request["nome"];
+    }
+    if($request["tipo"] != ""){
+      $condizioni = $condizioni."AND tipo = ? ";
+      $binds[] = $request["tipo"];
+    }
+    if($request["colore"] != ""){
+      $tabelle = $tabelle.", costi_carte";
+      $codizioni = $condizioni."AND costi_carte.carte_id = carte.id ";
+      $condizioni = $condizioni."AND colori_id = ? ";
+      $binds[] = $request["colore"];
+    }
+    if($request["espansione"] != ""){
+      $tabelle = $tabelle.", espanioni_carte";
+      $condizioni = $condizioni."AND espansioni_carte.carte_id = carte.id ";
+      $condizioni = $condizioni."AND espansioni_id = ? ";
+      $binds[] = $request["espansione"];
+    }
+    if($request["rarita"] != ""){
+      $condizioni = $condizioni."AND rarita = ? ";
+      $binds[] = $request["rarita"];
+    }
+
+    $query = $query.$tabelle." WHERE ".$condizioni;
+    echo($query);
+    $stm = $conn->prepare($query);
+
+    $i = 1;
+    foreach ($binds as $campo) {
+      $stm->bindParam($i, $campo);
+      $i = $i+1;
+    }
 
 
+    try{
+        $stm->execute();
+    }catch(PDOException $e){
+      echo($e->getMessage());
+    }
+    while($stm->fetch()) echo "Haloa";
+
+    return codificaRisultati($stm);
+  }
+
+  $arr["nome"] = "";
+  $arr["tipo"] = "";
+  $arr["colore"] = "";
+  $arr["espansione"] = "";
+  $arr["rarita"] = "";
+
+  $jsonResults = ricerca($arr);
 
 
-  $jsonResults = ricercaAttirbuti("espansioni");
+  /*$jsonResults = ricercaAttirbuti("espansioni");
 
   /*Leggo ricorsivamente l'array json, utile per gli array annidati*/
   $jsonIterator = new RecursiveIteratorIterator(
